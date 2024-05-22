38341cdca5830fa6a0a3efc14767e293294aaa70
NoelJosephPatricP-22 <noyaljpc.20@gmail.com>
2024-05-22 14:22:38 +0530

    Create Print message.py
    Program to print a message in Python

---
+++ basic/Print message.py
@@ -0,0 +1,7 @@
+#print('Hi how are you'): This line prints the string "Hi how are you". It's a greeting asking how someone is doing
+print('Hi how are you')
+#print('Hello!'): This line prints the string "Hello!". It's a simple greeting.
+print('Hello!')
+#print('Python'): This line prints the string "Python". It could refer to the programming language Python, or it could just be printing the word "Python" for any other reason
+print('Python')
+#Each print() statement outputs the specified string to the console when the script is executed. So, when you run this script, you'll see each of these lines printed one after the other, with each string displayed as specified.
