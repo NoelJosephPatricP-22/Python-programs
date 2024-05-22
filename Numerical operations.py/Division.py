a08d0b6496b44cc53accfa5a50748cf1885c8b2f
NoelJosephPatricP-22 <noyaljpc.20@gmail.com>
2024-05-22 15:46:05 +0530

    Create Division.py
    This code is a simple Python script that prompts the user to input two numbers, divides the first number by the second number, and then prints the result.

---
+++ Numerical operations.py/Division.py
@@ -0,0 +1,10 @@
+#number1=float(input('Enter the fisrt number:')): This line prompts the user to enter the first number. The input() function takes user input as a string, which is then converted to a floating-point number using the float() function to allow for decimal input. The entered value is stored in the variable number
+number1=float(input('Enter the fisrt number:'))
+#number2=float(input('Enter the second number:')): Similar to the previous line, this line prompts the user to enter the second number. The input is converted to a floating-point number and stored in the variable number2
+number2=float(input('Enter the second number:'))
+
+#division=number1/number2: This line calculates the division of number1 by number2 and stores the result in the variable division.
+division=number1/number2
+
+#print('Division of number1 by number2:',division): Finally, this line prints out the result of the division. It displays a message 'Division of number1 by number2:' followed by the value of division.
+print('Division of number1 by number2:',division)
