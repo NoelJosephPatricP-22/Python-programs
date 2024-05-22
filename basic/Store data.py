804430a0eba952a84a45d6a78f78b78b47032693
NoelJosephPatricP-22 <noyaljpc.20@gmail.com>
2024-05-22 14:38:41 +0530

    Create Store data.py
    This is a simple python script to store data to a variable and print the stored data

---
+++ basic/Store data.py
@@ -0,0 +1,14 @@
+#x='Python': This line assigns the string "Python" to the variable x.
+x='Python'
+#y='Java': This line assigns the string "Java" to the variable y.
+y='Java'
+#z='C++': This line assigns the string "C++" to the variable z
+z='C++'
+
+#print(x): This line prints the value of the variable x, which is "Python".
+print(x)
+#print(y): This line prints the value of the variable y, which is "Java".
+print(y)
+#print(z): This line prints the value of the variable z, which is "C++".
+print(z)
+
