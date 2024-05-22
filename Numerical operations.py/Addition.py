45e5b59e64db36462a29dc8201b2e2b5f4a6677a
NoelJosephPatricP-22 <noyaljpc.20@gmail.com>
2024-05-22 15:14:03 +0530

    Create Addition.py
    This code snippet prompts the user to input two numbers, adds them together, and then prints the result

---
+++ Numerical operations.py/Addition.py
@@ -0,0 +1,8 @@
+#number1 = float(input('Enter the first number:')): This line prompts the user to enter the first number. The input() function takes the user's input as a string, and float() converts it into a floating-point number to allow for decimal input. The entered value is stored in the variable number1.
+number1=float(input('Enter the first number:'))
+#number2 = float(input('Enter the second number:')): This line does the same as the first line, but it prompts the user to enter the second number. The entered value is stored in the variable number2.
+number2=float(input('Enter the second number:'))
+#sum = number1 + number2: This line calculates the sum of the two numbers entered by the user and stores the result in the variable sum.
+sum=number1+number2
+#print('Sum of two numbers:', sum): This line prints the sum of the two numbers. The text "Sum of two numbers:" is displayed followed by the value stored in the variable sum.
+print('Sum of two digits:',sum)
