c8bf71342ad185a656315ead546648fb77d4f1cb
NoelJosephPatricP-22 <noyaljpc.20@gmail.com>
2024-05-24 12:43:24 +0530

    Multiplication.py
    This program prompts you to enter two numbers, multiplies them together, and then prints out the product of the two numbers.

--- Numerical operations.py/Multiplication.py
+++ Numerical operations.py/Multiplication.py
@@ -1,10 +1,11 @@
 #number1 = float(input('Enter the first number:')): This line prompts the user to input the first number. The input() function takes the user's input as a string, and float() converts it into a floating-point number to allow for decimal input. The entered value is then stored in the variable number1.
-number1 = float(input('Enter the first number:'))
+Number1 = float(input('Enter the first number:'))
+
 #number2 = float(input('Enter the second number:')): Similar to the previous line, this line prompts the user to input the second number, converts it into a floating-point number, and stores it in the variable number2.
-number2 = float(input('Enter the second number:'))
+Number2 = float(input('Enter the second number:'))
 
 #product = number1 * number2: This line calculates the product of the two numbers entered by the user and stores the result in the variable product. It multiplies number1 by number2.
-product = number1 * number2
+Product = Number1 * Number2
 
 #print('Product of two numbers:', product): This line prints the product of the two numbers. The text "Product of two numbers:" is displayed followed by the value stored in the variable product, which is the result of the multiplication operation.
-print('Product of two numbers:',product)
+print('Product of two numbers:',Product)
