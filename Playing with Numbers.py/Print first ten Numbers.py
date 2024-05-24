a8e7400f8d1a8074f248cc6d9c1a7a705a4ca92a
NoelJosephPatricP-22 <noyaljpc.20@gmail.com>
2024-05-24 15:49:57 +0530

    Print first ten Numbers.py
    This code is a Python loop that iterates over a sequence of numbers from 1 to 10 (inclusive) and prints each number in the sequence.

---
+++ Playing with Numbers.py/Print first ten Numbers.py
@@ -0,0 +1,10 @@
+
+#for loop: The loop is constructed using the for keyword, which is used for iterating over a sequence (here, the sequence of numbers from 1 to 10).
+#range(1, 11): This function generates a sequence of numbers starting from the first argument (1 in this case) up to, but not including, the second argument (11 in this case). So, range(1, 11) generates the sequence [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].
+#i: This variable represents each individual element in the sequence as the loop iterates.
+for  i in range(1,11):
+#This line prints the current value of i in each iteration of the loop. Since i takes on the values from 1 to 10 (inclusive) as the loop iterates, it prints each number in that range.  
+  print(i)
+ #Iteration: The loop iterates over each value in the specified range, executing the body of the loop for each value of i.
+
+#Output: As a result, when you run this code, it will print the numbers 1 through 10, each on a separate line.
