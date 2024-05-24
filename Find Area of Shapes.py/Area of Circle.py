ea68877279653e07abea12004be7aab2d5897eaa
NoelJosephPatricP-22 <noyaljpc.20@gmail.com>
2024-05-24 12:24:56 +0530

    Area of Circle.py

---
+++ Find Area of Shapes.py/Area of Circle.py
@@ -0,0 +1,8 @@
+#r=float(input('Enter the radius:')): This line prompts the user to enter the radius of a circle. The input() function takes input from the user, and whatever the user types is treated as a string. Then, float() converts this string into a floating-point number, which allows for decimal input. The entered radius is stored in the variable r
+r=float(input('Enter the radius:'))
+
+#area=3.14*r*r: This line calculates the area of a circle using the formula 𝜋×𝑟^, where 𝜋 is approximately 3.14 and r is the radius entered by the user. The result is stored in the variable area.
+area=3.14*r*r
+
+#print('\nArea of circle is:',area): This line prints the calculated area of the circle. The \n in the string adds a newline character, so the output starts on a new line. Then, it prints "Area of circle is:" followed by the value stored in the variable area.
+print('\nArea of circle is:',area)
