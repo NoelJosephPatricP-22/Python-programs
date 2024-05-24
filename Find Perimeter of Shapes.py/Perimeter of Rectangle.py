ce1169f5cab87517b14644d1df87f40eaa1d6759
NoelJosephPatricP-22 <noyaljpc.20@gmail.com>
2024-05-24 13:25:54 +0530

    Perimeter of Rectangle.py
    This Python code calculates the perimeter of a rectangle based on the length and breadth entered by the user.

--- Find Perimeter of Shapes.py/Perimeter of Rectangle.py
+++ Find Perimeter of Shapes.py/Perimeter of Rectangle.py
@@ -5,8 +5,8 @@ Length=float(input('Enter the length of the rectangle:'))
 #Breadth=float(input('Enter the breadth of the rectangle:')): This line prompts the user to input a value for the breadth of the rectangle. Similar to the previous input line, it converts the user input to a floating-point number using the float() function and assigns it to the variable Breadth.
 Breadth=float(input('Enter the breadth of the rectangle:'))
 
-#Area=2*(Length+Breadth): This line calculates the perimeter of the rectangle using the formula: Perimeter=2×(Length+Breadth). It adds the length (Length) and breadth (Breadth) of the rectangle, multiplies the sum by 2, and assigns the result to the variable Area.
-Area=2*(Length+Breadth)
+#perimeter=2*(Length+Breadth): This line calculates the perimeter of the rectangle using the formula: Perimeter=2×(Length+Breadth). It adds the length (Length) and breadth (Breadth) of the rectangle, multiplies the sum by 2, and assigns the result to the variable Area.
+Perimeter=2*(Length+Breadth)
 
-#print('Perimeter of the rectangle is:',Area): This line prints out the calculated perimeter of the rectangle. It uses the print() function to display the text "Perimeter of the rectangle is:" followed by the value of the Area variable.
-print('Perimeter of the rectangle is:',Area)
+#print('Perimeter of the rectangle is:',Perimeter): This line prints out the calculated perimeter of the rectangle. It uses the print() function to display the text "Perimeter of the rectangle is:" followed by the value of the Area variable.
+print('Perimeter of the rectangle is:',Perimeter)
