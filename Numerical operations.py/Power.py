3664dd6a677cb2cab7b1d310ed54918a52a70f05
NoelJosephPatricP-22 <noyaljpc.20@gmail.com>
2024-05-24 12:46:28 +0530

    Power.py
    This Python code snippet prompts the user to enter a base number and a power value. Then, it calculates the result of raising the base number to the power entered by the user and prints out the result.

--- Numerical operations.py/Power.py
+++ Numerical operations.py/Power.py
@@ -1,10 +1,11 @@
 #power_value=float(input('Enter the fisrt number:')): This line prompts the user to enter the first number. The input() function takes user input as a string, which is then converted to a floating-point number using the float() function to allow for decimal input. The entered value is stored in the variable number1.
-power_value=float(input('Enter  the power:'))
+Power_value=float(input('Enter  the power:'))
+
 #number1=float(input('Enter the fisrt number:')): This line prompts the user to enter the first number. The input() function takes user input as a string, which is then converted to a floating-point number using the float() function to allow for decimal input. The entered value is stored in the variable number1.
-number1=float(input('Enter the number:'))
+Number1=float(input('Enter the number:'))
 
 #Calculate the power of any number of your entered choice
-power=number1**power
+Power=Number1**Power
 
-#print the variable power
-print('The answer is:','power)
+# This line prints out the calculated power. It uses the print() function to display the text "The answer is:" followed by the value of the variable Power.
+print('The answer is:',Power)
