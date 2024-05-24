70594096e7b49234c9d48bca89d3bf8dd8049a21
NoelJosephPatricP-22 <noyaljpc.20@gmail.com>
2024-05-24 13:05:34 +0530

    Reading an Input.py

---
+++ Python Basics.py/Reading an Input.py
@@ -0,0 +1,8 @@
+
+#input('Enter your Name:'): This prompts the user to input their name just like before
+input=input('Enter your Name:')
+
+#print('Hi',input): This line prints a greeting followed by the name entered by the user
+print('Hi',input)
+
+#So, when you run this script, it prompts you to enter your name, then it prints "Hi" followed by the name you entered. For example, if you entered "John", it would print "Hi John"
