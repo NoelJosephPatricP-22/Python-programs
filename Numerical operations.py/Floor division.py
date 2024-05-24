5542d02a7e25be5b3adfe0d982a62d24277ba137
NoelJosephPatricP-22 <noyaljpc.20@gmail.com>
2024-05-24 12:42:18 +0530

    Floor division.py
    1.Floor division is a division operation that rounds the result down to the nearest whole number or integer, which is less than or equal to the normal division result.
    2. Floor division divides the left operand by the right operand and returns the largest integer less than or equal to the quotient.
    3.when you run this script, it will ask you to input two numbers, perform floor division of the first number by the second number, and then display the result.

--- Numerical operations.py/Floor division.py
+++ Numerical operations.py/Floor division.py
@@ -1,12 +1,13 @@
 '''Floor division is a division operation that rounds the result down to the nearest whole number or integer, which is less than or equal to the normal division result'''
 
 #number1=float(input('Enter the fisrt number:')): This line prompts the user to enter the first number. The input() function takes user input as a string, which is then converted to a floating-point number using the float() function to allow for decimal input. The entered value is stored in the variable number1.
-number1=float(input('Enter the fisrt number:'))
+Number1=float(input('Enter the fisrt number:'))
+
 #number2=float(input('Enter the second number:')): Similar to the previous line, this line prompts the user to enter the second number. The input is converted to a floating-point number and stored in the variable number2.
-number2=float(input('Enter the second number:'))
+Number2=float(input('Enter the second number:'))
 
 #floordivision=number1//number2: This line calculates the floor division of number1 by number2 and stores the result in the variable floordivision
-floordivision=number1//number2
+Floordivision=Number1//Number2
 
 #print('Division of number1 by number2:',floordivision): Finally, this line prints out the result of the floor division. It displays a message 'Division of number1 by number2:' followed by the value of floordivision.
-print('Floor Division of number1 by number2:',floordivision)
+print('Floor Division of number1 by number2:',Floordivision)
