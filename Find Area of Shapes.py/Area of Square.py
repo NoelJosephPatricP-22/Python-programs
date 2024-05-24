ee1a4ad166257c14f7d607db37a80f28001b4a59
NoelJosephPatricP-22 <noyaljpc.20@gmail.com>
2024-05-24 12:35:59 +0530

    Area of Square.py
    This Python code prompts the user to input a value for the side length of a square. It reads the input as a floating-point number and assigns it to the variable side1. Then, it calculates the area of the square by squaring the value of side1 and assigns it to the variable area. Finally, it prints out the calculated area.

--- Find Area of Shapes.py/Area of Square.py
+++ Find Area of Shapes.py/Area of Square.py
@@ -2,7 +2,7 @@
 Side1=float(input('Enter the value:'))
 
 #area=side1*side1: This line calculates the area of the square by multiplying the side length (side1) by itself, which is equivalent to squaring the side length. The result is assigned to the variable area.
-Area=Side1*side1
+Area=Side1*Side1
 
 #print('Area of the square is:',area): This line prints out the calculated area of the square. It uses the print() function to display the text "Area of the square is:" followed by the value of the area variable.
 print('Area of the square is:',Area)
