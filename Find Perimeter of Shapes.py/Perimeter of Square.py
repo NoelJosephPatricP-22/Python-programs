9e0b9cf2f151bd9b7978ae4fde7efff63a0e2555
NoelJosephPatricP-22 <noyaljpc.20@gmail.com>
2024-05-24 13:24:02 +0530

    Perimeter of Square.py
    This Python code calculates the perimeter of a square based on the length of its side entered by the user.

---
+++ Find Perimeter of Shapes.py/Perimeter of Square.py
@@ -0,0 +1,9 @@
+
+#Side=float(input('Enter the value of the side of the square:')): This line prompts the user to input a value for the side length of the square. The input() function takes user input as a string, which is then converted to a floating-point number using the float() function and assigned to the variable Side.
+Side=float(input('Enter the value of the side of the square:'))
+
+#Perimeter=4*Side: This line calculates the perimeter of the square. In a square, all sides are equal, so to find the perimeter, you multiply the length of one side (Side) by 4. The result is assigned to the variable Perimeter.
+Perimeter=4*Side
+
+#print('Perimeter of the square is:',Perimeter): This line prints out the calculated perimeter of the square. It uses the print() function to display the text "Perimeter of the square is:" followed by the value of the Perimeter variable.
+print('Perimeter of the square is:',Perimeter)
